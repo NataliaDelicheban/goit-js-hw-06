cdfc64759448938fb14e97496dca65f64e06f1b0
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-05-16 16:46:50 +0200

    create task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColor = document.querySelector("body")
+const btnChange = document.querySelector(".change-color")
+const spanColor = document.querySelector(".color")
+
+function changeColor(event) {
+  bodyColor.style.backgroundColor = getRandomHexColor(event);
+  return spanColor.textContent = getRandomHexColor.value;
+
+}
+bodyColor.addEventListener("click", changeColor)
