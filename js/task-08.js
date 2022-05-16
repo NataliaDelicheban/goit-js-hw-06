536baeadfbb76c385152ded184d639b3ca9f5198
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-05-16 16:07:24 +0200

    create task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", fullForm);
+ 
+function fullForm(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please, fill in all lines!");
+    }
+    else {
+        const formData = {
+            email, password,
+        };
+    
+                console.log(formData);
+        }
+        event.currentTarget.reset();
+    }
+
 
