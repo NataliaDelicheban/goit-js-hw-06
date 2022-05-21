380a429d6389ba8521eb17a598eaafde98e6aa0a
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-05-21 17:05:36 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -6,8 +6,8 @@ const bodyColor = document.querySelector("body")
 const btnChange = document.querySelector(".change-color")
 const spanColor = document.querySelector(".color")
 
-function changeColor(event) {
-  bodyColor.style.backgroundColor = getRandomHexColor(event);
+function changeColor() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
   spanColor.textContent = bodyColor.style.backgroundColor;
 }
 bodyColor.addEventListener("click", changeColor)
